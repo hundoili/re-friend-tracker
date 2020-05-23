3ea5872d0fd41eda3cd2ae53f8ed6f5ecda4e696
hundoili <hundoili@students.zhaw.ch>
2020-05-23 11:55:59 +0000

    add group form

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -125,6 +125,33 @@ export class GuiModel {
                         }
                     ]
                 },
+                {
+                    "id": "GroupForm",
+                    "title": "Group",
+                    "url": "/group",
+                    "formFieldList": [
+                        {
+                            "id": "name",
+                            "type": "text",
+                            "name": "GroupName",
+                            "width": 2,
+                            "required": true
+                        },
+                        {
+                            "type": "deleteButton",
+                            "name": "Delete"
+                        },
+                        {
+                            "type": "cancelButton",
+                            "name": "Cancel"
+                        },
+                        {
+                            "type": "okButton",
+                            "name": "Ok"
+                        }
+                    ]
+                }
+
             ],
             "pageList": [
                 {
